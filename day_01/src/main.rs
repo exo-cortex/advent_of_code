4747dfd221369d438083bf3019a2beb856cac89a
lukipuki <lukipuki@physik.tu-berlin.de>
2023-12-07 22:34:50 +0100

    Add needed files for first part of problem.

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,21 +1,35 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn main() {
-    let file = File::open("./example.txt").unwrap();
-    let reader = BufReader::new(file);
+fn task_1(reader: &mut BufReader<File>) -> u32 {
+    let mut sum = 0;
 
     for line in reader.lines() {
         let line = line.unwrap();
-        print!("{}, ", &line);
-        let numbers = &line
+        // print!("{}, ", &line);
+        let found_numbers = &line
             .chars()
             .filter(|c| c.is_digit(10))
-            .map(|c| (*c.to_string()).parse::<i32>().unwrap())
-            .collect::<Vec<i32>>();
-        print!("{:?}, ", numbers);
-        let first = numbers.first().unwrap();
-        let last = numbers.last().unwrap();
-        println!("[first: {}, last: {}]", first, last);
+            .collect::<Vec<char>>();
+        let combined_number = format!(
+            "{}{}",
+            found_numbers.first().unwrap().to_string(),
+            found_numbers.last().unwrap().to_string()
+        )
+        .parse::<u32>()
+        .unwrap();
+
+        sum += combined_number;
     }
+    sum
+}
+
+fn main() {
+    let mut example_input = BufReader::new(File::open("./example_1.txt").unwrap());
+    let mut full_input = BufReader::new(File::open("./puzzle_input.txt").unwrap());
+
+    println!("example data: total sum is {}", task_1(&mut example_input));
+
+    let task_a_number = task_1(&mut full_input);
+    println!("input: total sum is {}", task_a_number);
 }
